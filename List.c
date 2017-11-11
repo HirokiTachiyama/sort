a03ca6b9767647b7f0189382fac06c36ff8137f8
HirokiTachiyama <hirokitachiyama@gmail.com>
2017-11-11 22:39:03 +0900

    dev on CLion

--- List.c
+++ List.c
@@ -6,36 +6,39 @@
 
 
 //Listの初期化
-//先頭ノードは文字列格納には使用しない。
 List* List_initialize() {
     List* top_node = (List*)malloc(sizeof(List));
-    top_node->next_node = NULL;
-    top_node->str = NULL;
+    top_node->next_node = (List*)malloc(sizeof(List*));
+	top_node->str = (char*)malloc(sizeof(char) * NUM_CHARACTERS);
+	printf("top node is initialized\n");
 	return top_node;
 }
 
 
-//引数のListの最後のノードの後ろに新たに生成したノードを繋げる
+//引数のListの最後のノードの次に新たに生成したノードを繋げる
 void List_add_new_node (List* _lists) {
-  List* last_node = List_get_last_node(_lists);
+  //新しいノードの準備
   List* new_node = (List*)malloc(sizeof(List));
+  new_node->next_node = (List*)malloc(sizeof(List*));
+  new_node->str = (char*)malloc(sizeof(char) * NUM_CHARACTERS);
 
-  last_node->next_node = new_node;
+  List* last_node_of_lists = List_get_last_node(_lists);
+  last_node_of_lists->next_node = new_node;
+  printf("new node is added\n");
 }
 
 void List_set_string_to_last_node(List* _lists, char* _str) {
-
-  printf("%d\n", NUM_CHARACTERS);
-  printf("New List was added3.\n");
-  //strcpy(new_node->str, _str);
-  printf("New List was added3.\n");
-  
+  printf("hoge~\n");
+  List* last_node = List_get_last_node(_lists);
+  printf("hoge~\n");
+  strcpy(last_node->str, _str);
+  printf("New str:%s was set.\n", last_node->str);
 }
 
 int List_get_size(List* lists) {
   int size = 0;
   List* tmp = lists;
-
+  
   while(tmp != NULL) {
 	tmp = lists->next_node;
 	size++;
@@ -45,9 +48,11 @@ int List_get_size(List* lists) {
 }
 
 List* List_get_last_node(List* lists) {
+  printf("nyoho");
   List* current_node = lists;
   List* next_node = lists->next_node;
   if (next_node == NULL) {
+	printf("this is top node\n");
 	return current_node;
   }
 
@@ -55,6 +60,7 @@ List* List_get_last_node(List* lists) {
 	current_node = next_node;
 	next_node = next_node->next_node;
 	if(next_node == NULL) {
+	  printf("last node is retturned\n");
 	  return current_node;
 	}
   }
--- workspace.c
+++ workspace.c
@@ -4,10 +4,10 @@
 int main(void) {
 
   List* lists = List_initialize();
-  List_add_new_node(lists);
   List_set_string_to_last_node(lists, "hoge");
 
   List_add_new_node(lists);
+
   List_set_string_to_last_node(lists, "fuga");
 
 
